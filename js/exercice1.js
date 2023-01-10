0c8a792c7006607698458d124cde19024d5512a0
Magara Etchien N'gouan <magaraetchien@live.fr>
2023-01-10 13:50:45 +0000

    DI BootCamp week2 Day1 XP_Gold

---
+++ js/exercice1.js
@@ -0,0 +1,14 @@
+/*   
+
+  Exercise 1 : Favorite Color
+Instructions
+Write some simple Javascript code that will join all the items in the array above, and console.log the result.
+
+*/
+
+let sentence = ["my","favorite","color","is","blue"];
+
+//answer
+
+let wordJoin = sentence[0] + " " + sentence[1] + " " + sentence[2] + " " + sentence[3] + " " + sentence[4];
+console.log(wordJoin);
